95ff601e2fb2164f640ce5b20a01084665367926
ShannonMiller <shans.miller@gmail.com>
2015-09-10 18:35:44 -0400

    fixed layout of brewery page

--- js/grandjquery.js
+++ js/grandjquery.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 		alert("Please come back when you're 21!")
 	
 
-});
+	});
 
 	 	$(".circle").hover(function(){
 		  $(this).css("width","70px")
@@ -22,26 +22,41 @@ $(document).ready(function(){
 		 });
 	
 	  	$("#item").hover(function(){
+	  		console.log("A")
 	  		$("#price").css("visibility", "visible");
-	 		$(this).faddClass("hidden");
-
+	 		//$(this).addClass("hidden-me");
+	 		 }, function(){
+	 	  	//$(this).removeClass("hidden-me")
+	 	  	$("#price").css("visibility","visible");
+	 	 	//$(this).addClass("hidden-me")
+	 	
 
 	 	 });
 	  	$("#itemB").hover(function(){
+	  		console.log("B")
+
 	  		$("#priceB").css("visibility", "visible");
-	 		$(this).addClass("hidden");
+	 		//$(this).addClass("hidden-me");
 
 
+	 	 }, function(){
+	 	 	//$(this).removeClass("hidden-me")
 	 	 });
 
 	  	$("#itemC").hover(function(){
+	  		console.log("C")
 	  		$("#priceC").css("visibility", "visible");
-	 		$(this).addClass("hidden");
+	 		//$(this).addClass("hidden-me");
+	 		}, function(){
+	 	 	//$(this).removeClass("hidden-me")
 
 	 	 });
-	  		$("#itemD").hover(function(){
+	  	$("#itemD").hover(function(){
+	  		console.log("D")
 	  		$("#priceD").css("visibility", "visible");
-	 		$(this).addClass("hidden");
+	 		//$(this).addClass("hidden-me");
+	 		}, function(){
+	 	 	//$(this).removeClass("hidden-me")
 
 	 	 });
 
